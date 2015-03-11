f59c5494e20d463f164e213f774d7dbb2f9da652
Kevin Wu <kevindwu@gmail.com>
2015-03-11 16:48:22 -0700

    added test coverage

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ var internals = {};
 internals.pluginName = 'hapiRouteAcl';
 
 exports.register = function (server, options, next) {
-  Hoek.assert(options.permissionsFunc, 'options.permissionsFunc is required');
-  Hoek.assert(typeof options.permissionsFunc === 'function', 'options.permissionsFunc must be a valid function');
-
-  internals.permissionsFunc = options.permissionsFunc;
-
-  server.ext('onPostAuth', internals.implementation);
-  next();
+  if (_.isUndefined(options.permissionsFunc)) {
+    next(new Error('options.permissionsFunc is required'));
+  } else if (!_.isFunction(options.permissionsFunc)) {
+    next(new Error('options.permissionsFunc must be a valid function'));
+  } else {
+    internals.permissionsFunc = options.permissionsFunc;
+    server.ext('onPostAuth', internals.implementation);
+    next();
+  }
 };
 
 exports.register.attributes = {
@@ -25,20 +27,16 @@ exports.register.attributes = {
 
 internals.implementation = function(request, reply) {
   if (!_.isEmpty(request.route.settings.plugins[internals.pluginName])) {
+
     var requiredPermissions = request.route.settings.plugins[internals.pluginName].permissions;
+
     if (!_.isEmpty(requiredPermissions)) {
       internals.permissionsFunc(request.auth.credentials, function(error, userPermissions) {
-        if (error) {
-          return reply(Boom.wrap(error));
+        var hasPermission = internals.checkPermissions(requiredPermissions, userPermissions);
+        if (hasPermission) {
+          return reply.continue();
         } else {
-          var result = internals.checkPermissions(requiredPermissions, userPermissions);
-          if (_.isError(result)) {
-            return reply(Boom.wrap(result));
-          } else if (result) {
-            return reply.continue();
-          } else {
-            return reply(Boom.unauthorized('Access denied'));
-          }
+          return reply(Boom.unauthorized('Access denied'));
         }
       });
     } else {
@@ -50,24 +48,33 @@ internals.implementation = function(request, reply) {
 };
 
 internals.checkPermissions = function(requiredPermissions, userPermissions) {
+  if (_.isString(requiredPermissions)) {
+    requiredPermissions = [requiredPermissions];
+  }
+
   var permissionMap = {};
 
   _.forEach(requiredPermissions, function(requiredPermission) {
+    Hoek.assert(_.isString(requiredPermission), 'permission must be a string');
+
     var parts = requiredPermission.split(':');
 
-    if (parts.length !== 2) {
-      return new Error('permission must match format: [routeName]:[read|create|edit|delete]');
-    } else {
-      var routeName = parts[0];
-      var crud = parts[1];
+    Hoek.assert(parts.length === 2, 'permission must be formatted: [routeName]:[read|create|edit|delete]');
 
+    var routeName = parts[0];
+    var crud = parts[1];
+
+    if (_.isUndefined(userPermissions[routeName]) || _.isUndefined(userPermissions[routeName][crud])) {
+      permissionMap[requiredPermission] = false;
+    } else {
       permissionMap[requiredPermission] = userPermissions[routeName][crud];
     }
   });
 
-  var result = _.reduce(permissionMap, function(result, permission, requiredPermission) {
-    return result && permissionMap[requiredPermission];
-  }, true);
+  var hasPermission = _.reduce(permissionMap, function(result, permission) {
+    result = result && permission;
+    return result;
+  });
 
-  return result;
+  return hasPermission;
 };
---
+++ test/index.js
@@ -0,0 +1,328 @@
+'use strict';
+
+// Load modules
+
+var Code = require('code');
+var Lab = require('lab');
+var Hapi = require('hapi');
+
+// Declare internals
+
+var internals = {};
+
+internals.permissionsFunc = function(credentials, callback) {
+  var userPermissions = {
+    cars: {
+      read: true,
+      create: false,
+      edit: true,
+      delete: true
+    },
+    drivers: {
+      read: true,
+      create: false,
+      edit: false,
+      delete: false
+    },
+    abilities: {
+      read: false,
+      create: false,
+      edit: false,
+      delete: false
+    }
+  };
+
+  callback(null, userPermissions);
+};
+
+// Test shortcuts
+
+var lab = exports.lab = Lab.script();
+var before = lab.before;
+var beforeEach = lab.beforeEach;
+var after = lab.after;
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+
+describe('hapi-route-acl', function() {
+
+  describe('registration', function() {
+    var server;
+
+    beforeEach(function(done) {
+      server = new Hapi.Server();
+      server.connection();
+      done();
+    });
+
+    it('should return an error if options.permissionsFunc is not defined', function(done) {
+      server.register({
+        register: require('./../')
+      }, function(err) {
+        expect(err).to.exist();
+        done();
+      });
+    });
+
+    it('should return an error if options.permissionsFunc is not a function', function(done) {
+      server.register({
+        register: require('./../'),
+        options: {
+          permissionsFunc: 123
+        }
+      }, function(err) {
+        expect(err).to.exist();
+        done();
+      });
+    });
+
+  });
+
+  describe('route protection', function() {
+    var server;
+
+    beforeEach(function(done) {
+      server = new Hapi.Server();
+      server.connection();
+      server.register({
+        register: require('./../'),
+        options: {
+          permissionsFunc: internals.permissionsFunc
+        }
+      }, function(err) {
+        if (err) {
+          throw err;
+        }
+      });
+      done();
+    });
+
+    it('should allow access to a route if plugin configuration is not defined in route config', function(done) {
+      server.route({
+        method: 'GET',
+        path: '/unprotected1',
+        config: {
+          handler: function(request, reply) {
+            reply('hola mi amigo');
+          }
+        }
+      });
+      server.inject({
+        method: 'GET',
+        url: '/unprotected1'
+      }, function(res) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('should allow access to a route if required permission array is empty', function(done) {
+      server.route({
+        method: 'GET',
+        path: '/unprotected2',
+        config: {
+          handler: function(request, reply) {
+            reply('como estas?');
+          },
+          plugins: {
+            hapiRouteAcl: {
+              permissions: []
+            }
+          }
+        }
+      });
+      server.inject({
+        method: 'GET',
+        url: '/unprotected2'
+      }, function(res) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('should allow access to a route if user has permission', function(done) {
+      server.route({
+        method: 'GET',
+        path: '/cars',
+        config: {
+          handler: function(request, reply) {
+            reply(['Toyota Camry', 'Honda Accord', 'Ford Fusion']);
+          },
+          plugins: {
+            hapiRouteAcl: {
+              permissions: ['cars:read']
+            }
+          }
+        }
+      });
+      server.inject({
+        method: 'GET',
+        url: '/cars'
+      }, function(res) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('should allow access for permissions defined as a string', function(done) {
+      server.route({
+        method: 'GET',
+        path: '/cars/{id}',
+        config: {
+          handler: function(request, reply) {
+            reply('Toyota Camry');
+          },
+          plugins: {
+            hapiRouteAcl: {
+              permissions: 'cars:read'
+            }
+          }
+        }
+      });
+      server.inject({
+        method: 'GET',
+        url: '/cars/1'
+      }, function(res) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('should deny access to a route if user does not have permission', function(done) {
+      server.route({
+        method: 'POST',
+        path: '/cars',
+        config: {
+          handler: function(request, reply) {
+            reply('car created!');
+          },
+          plugins: {
+            hapiRouteAcl: {
+              permissions: ['cars:create']
+            }
+          }
+        }
+      });
+      server.inject({
+        method: 'POST',
+        url: '/cars'
+      }, function(res) {
+        expect(res.statusCode).to.equal(401);
+        done();
+      });
+    });
+
+    // it('should throw an error if route permission is not a string', function(done) {
+    //   try {
+    //     internals.server.inject({
+    //       method: 'PUT',
+    //       url: '/cars/1'
+    //     }, function(res) {
+    //       expect(res.statusCode).to.equal(500);
+    //       done();
+    //     });
+    //   } catch(error) {
+    //     console.log('error caught');
+    //   }
+    // });
+
+    it('should deny access to a route if user permission is not defined for the route', function(done) {
+      server.route({
+        method: 'DELETE',
+        path: '/foobar/{id}',
+        config: {
+          handler: function(request, reply) {
+            reply('car deleted!');
+          },
+          plugins: {
+            hapiRouteAcl: {
+              permissions: ['foobar:delete']
+            }
+          }
+        }
+      });
+      server.inject({
+        method: 'DELETE',
+        url: '/foobar/1'
+      }, function(res) {
+        expect(res.statusCode).to.equal(401);
+        done();
+      });
+    });
+
+    it('should allow access to a route with multiple permission requirements if user has permissions', function(done) {
+      server.route({
+        method: 'GET',
+        path: '/cars/{id}/drivers',
+        config: {
+          handler: function(request, reply) {
+            reply(['Greg', 'Tom', 'Sam']);
+          },
+          plugins: {
+            hapiRouteAcl: {
+              permissions: ['cars:read', 'drivers:read']
+            }
+          }
+        }
+      });
+      server.inject({
+        method: 'GET',
+        url: '/cars/1/drivers'
+      }, function(res) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('should deny access to a route with two permission requirements if user does not have permissions', function(done) {
+      server.route({
+        method: 'DELETE',
+        path: '/cars/{carId}/drivers/{driverId}',
+        config: {
+          handler: function(request, reply) {
+            reply('driver deleted!');
+          },
+          plugins: {
+            hapiRouteAcl: {
+              permissions: ['drivers:delete', 'cars:read']
+            }
+          }
+        }
+      });
+      server.inject({
+        method: 'DELETE',
+        url: '/cars/1/drivers/1'
+      }, function(res) {
+        expect(res.statusCode).to.equal(401);
+        done();
+      });
+    });
+
+    it('should deny access to a route with multiple permission requirements if user does not have permissions', function(done) {
+      server.route({
+        method: 'GET',
+        path: '/cars/{carId}/drivers/{driverId}/abilities/{abilitiesId}',
+        config: {
+          handler: function(request, reply) {
+            reply('driver deleted!');
+          },
+          plugins: {
+            hapiRouteAcl: {
+              permissions: ['drivers:read', 'cars:read', 'abilities:read']
+            }
+          }
+        }
+      });
+      server.inject({
+        method: 'GET',
+        url: '/cars/1/drivers/1/abilities/1'
+      }, function(res) {
+        expect(res.statusCode).to.equal(401);
+        done();
+      });
+    });
+
+  });
+
+});
